5d9f3c8b36a9092ad024a5a63b9f1555d95dac95
askanakmala <46845990+askanakmala@users.noreply.github.com>
2020-10-14 06:59:32 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,24 +15,38 @@
 # limitations under the License.
 #
 
-DEVICE_PATH := device/xiaomi/olivewood
+PLATFORM_PATH := device/xiaomi/olivewood
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
 
+TARGET_NO_BOOTLOADER := true
+TARGET_BOOTLOADER_BOARD_NAME := sdm439
+TARGET_BOARD_PLATFORM := sdm439
+
 # Architecture
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := generic
+TARGET_CPU_ABI := armeabi
+TARGET_CPU_VARIANT := cortex-a15
+
+TARGET_BOARD_PLATFORM_GPU := qcom-andreno505
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := olivewood
 
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x4000000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x4000000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x100000000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x550647000
+BOARD_CACHEIMAGE_PARTITION_SIZE := 0x18000000
+BOARD_PERSISTIMAGE_PARTITION_SIZE := 0x2000000
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+
 # File systems
 BOARD_HAS_LARGE_FILESYSTEM := true
-#BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864 # This is the maximum known partition size, but it can be higher, so we just omit it
 BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
 BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
@@ -40,15 +54,21 @@ TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 TARGET_COPY_OUT_VENDOR := vendor
 
+# Cmdline
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8
+BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237 ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE += androidboot.bootdevice=7824900.sdhci androidboot.usbconfigfs=true androidboot.configfs=true
+BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 earlycon=msm_hsl_uart,0x78B0000 vmalloc=300M firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7
+#BOARD_KERNEL_CMDLINE += androi.dboot.selinux=permissive
+#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlycon=msm_hsl_uart,0x78B0000 vmalloc=300M firmware_class.path=/vendor/firmware_mnt/image androidboot.usbconfigfs=true loop.max_part=7 androidboot.selinux=permissive
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/zImage-dtb
 BOARD_BOOTIMG_HEADER_VERSION := 1
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_RAMDISK_OFFSET := 0x01000000
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
-BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
 BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
@@ -60,16 +80,72 @@ TARGET_KERNEL_CONFIG := olivewood_defconfig
 # Ramdisk compression
 LZMA_RAMDISK_TARGETS := recovery
 
+# Fstab
+TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/recovery.fstab
+
 # Platform
 TARGET_BOARD_PLATFORM := msm8937
+BOARD_USES_QCOM_HARDWARE := true
+
+# AVB
+BOARD_AVB_ENABLE := true
+BOARD_AVB_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
+
+# Filesystem
+#TARGET_USERIMAGES_USE_F2FS := true
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR := vendor
+
+# Crypto
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_CRYPTO_FBE := true
+TW_INCLUDE_FBE_METADATA_DECRYPT := true
+BOARD_USES_METADATA_PARTITION := true
+
 
 # Hack: prevent anti rollback
-PLATFORM_SECURITY_PATCH := 2099-12-31
+PLATFORM_SECURITY_PATCH := 2025-12-31
 PLATFORM_VERSION := 16.1.0
 
 # TWRP Configuration
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TW_THEME := portrait_hdpi
-TW_EXTRA_LANGUAGES := true
-TW_SCREEN_BLANK_ON_BOOT := true
-TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
+HAVE_SELINUX := true
+RECOVERY_SDCARD_ON_DATA := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_NO_LEGACY_PROPS := true
+TW_EXTRA_LANGUAGES := true
+TW_INCLUDE_NTFS_3G := true
+TW_Y_OFFSET := 57
+TW_H_OFFSET := -57
+TW_SCREEN_BLANK_ON_BOOT := false
+TWRP_INCLUDE_LOGCAT := true
+TARGET_USES_LOGD := true
+TW_NO_USB_STORAGE := true
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_MAX_BRIGHTNESS := 2047
+TW_DEFAULT_BRIGHTNESS := 489
+TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
+
+# aditonal
+TW_DEVICE_VERSION=$(shell date '+%Y%m%d')
+
+# System-as-root
+
+#BOARD_BUILD_SYSTEM_ROOT_IMAGE := true //set is system.prop
+#BOARD_ROOT_EXTRA_FOLDERS := bluetooth dsp firmware persist
+#BOARD_SUPPRESS_SECURE_ERASE := true
+# OTA
+
+TARGET_OTA_ASSERT_DEVICE := olivewood
+
+# Treble
+#BOARD_NEEDS_VENDORIMAGE_SYMLINK := false
+#BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+#TARGET_COPY_OUT_VENDOR := vendor
